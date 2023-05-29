aa16bfc52448d2fc4f975085d4afc079616ae8c3
DeadlyWhiskers <133860751+DeadlyWhiskers@users.noreply.github.com>
2023-05-29 22:30:19 +0300

    Items collision are now fixed

--- Source/OOP_Project_Game/CC_PlayerClass.cpp
+++ Source/OOP_Project_Game/CC_PlayerClass.cpp
@@ -1,8 +1,8 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-#include "CC_PickableItem.h"
-
 #include "CC_PlayerClass.h"
+
+#include "CC_PickableItem.h"
 
 //to create simmilar classes
 //#include "BulletDef.h"
